069e32754f8cf0d81e917fe8ce5c6411d3f04be7
AtlanticMGH <hotzker12@gmail.com>
2023-04-20 15:47:51 +0200

    Update

--- alien.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class alien():
-    alienImg = pygame.image.load('alien.png')
-    ah = 100
-    aw = 63
---
+++ code/alien.py
@@ -0,0 +1,6 @@
+import pygame
+
+class alien():
+    alienImg = pygame.image.load('alien.png')
+    ah = 86
+    aw = 54
